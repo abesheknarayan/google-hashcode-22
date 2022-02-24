e7295b7f7de8e0eced23331e951a63b8e7b2086c
Vivek Raveendran <satsvivek@gmail.com>
2022-02-25 00:41:12 +0530

    build

---
+++ build.sh
@@ -0,0 +1,6 @@
+./bin/sol < tests/a_an_example.in.txt > output/a_an_example.out.txt
+./bin/sol < tests/b_better_start_small.in.txt > output/b_better_start_small.out.txt
+./bin/sol < tests/c_collaboration.in.txt > output/c_collaboration.out.txt
+./bin/sol < tests/d_dense_schedule.in.txt > output/d_dense_schedule.out.txt
+./bin/sol < tests/e_exceptional_skills.in.txt > output/e_exceptional_skills.out.txt
+./bin/sol < tests/f_find_great_mentors.in.txt > output/f_find_great_mentors.out.txt
